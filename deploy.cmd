e35bccb9d424010fcc6d4afbd5487a74d9de901c
xojins <xojins@gmail.com>
2017-07-20 12:55:33 +0000

    editing deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
-::mkdir ~/.npm-global
-::NPM_CONFIG_PREFIX=~/.npm-global
-::npm install -g yo generator-hubot --slient
+#::mkdir ~/.npm-global
+#::NPM_CONFIG_PREFIX=~/.npm-global
+#::npm install -g yo generator-hubot --slient
 
